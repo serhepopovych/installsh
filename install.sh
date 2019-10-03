serhepopovych/installsh

commit 189b402c1dfd96579d4ca0f15a8c73011f50cae0

installsh: Get rid of readlink specific -q and -m options
It seems -q implied by default and -m can be replaced with -f when all
but last component in path does exist that ensured with [ -L ... ]
conditional.

Signed-off-by: Serhey Popovych <serhe.popovych@gmail.com>

install.sh
@@ -232,7 +232,7 @@ same()
 	fi
 	# Symlinks and target is the same?
 	if [ -L "$d" -a -L "$s" -a \
-	     "$(readlink -q "$d")" = "$(readlink -q "$s")" ]; then
+	     "$(readlink "$d")" = "$(readlink "$s")" ]; then
 		return 0
 	fi
 	# Not a directory and content is the same?
@@ -372,7 +372,7 @@ install_sh__reg_file_copy()
 	local d="${2:?missing 2d arg to ${func}() (<dst>)}"
 
 	if [ -L "$s" ]; then
-		t="$(cd "$SP" && readlink -m "$s")" || return
+		t="$(cd "$SP" && readlink -f "$s")" || return
 		# Outside of SP directory?
 		subpath "$SP" "$t" t || return
 		# Make path relative
@@ -679,17 +679,20 @@ reg_file_copy()
 	local t
 
 	if [ -L "$s" ]; then
-		t="$(cd "$SP" && readlink -m "$s")" || return
+		t="$(cd "$SP" && readlink -f "$s")" || return
 		# Outside of SP directory?
 		subpath "$SP" "$t" t || return
 		# Subproject responsibility?
 		[ -n "${t##*/.subprojects/*}" ] || return 0
 		# Make path relative: we do not expect symlinks from DEST
 		# to ROOT as pointless and DEST installed before ROOT
-		t="$(cd "$DP" && readlink -m "$DP$t")" || return
-		# Outside of DP directory?
-		subpath "$DP" "$t" t || return
 		t="$DP$t"
+		if [ -L "$t" ]; then
+			t="$(cd "$DP" && readlink -f "$t")" || return
+			# Outside of DP directory?
+			subpath "$DP" "$t" t || return
+			t="$DP$t"
+		fi
 		[ -e "$t" -o ! -d "$s" ] || mkdir -p "$t" || return
 		relative_path "$t" "$d" s || return
 		# Backup if needed before installing
@@ -738,7 +741,7 @@ install_root()
 			[ -L "$l" ] || continue
 
 			# Is it's target is RD?
-			lnk="$(readlink -q "$l")"
+			lnk="$(readlink "$l")"
 			[ -z "${lnk##$RD/*}" ] || continue
 
 			# Then remove it

commit 189d565b0588a6adcf2655947d17807c191e9227

installsh: Do not assume dest to be symlink when canonicalize path
According to readlink(1) -f option description all but last components
must exist to resolve path successfuly. It might happen that symlink
destination path exists but not canonical and there is no link to read
(e.g. test -L "$l" is false).

To extend symlink path canonicalization check up to last component
existence to call readlink(1) and proceed with possibly non-canonical
path when that path does not exist. Later potentially can have negative
effects that source links will remain unresolved in destination if
deploy.sh -d option isn't "/".

Since we do not expect symlinks from dest to root and install_dest()
called prior to install_root() this should not affect us at all since
destination paths should already exist.

Fixes: 189b402c1dfd ("installsh: Get rid of readlink specific -q and -m options")
Signed-off-by: Serhey Popovych <serhe.popovych@gmail.com>

install.sh
@@ -689,7 +689,7 @@ reg_file_copy()
 		# Make path relative: we do not expect symlinks from DEST
 		# to ROOT as pointless and DEST installed before ROOT
 		t="$DP$t"
-		if [ -L "$t" ]; then
+		if [ -e "${t%/*}" ]; then
 			t="$(cd "$DP" && readlink -f "$t")" || return
 			# Outside of DP directory?
 			subpath "$DP" "$t" t || return