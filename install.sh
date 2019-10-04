serhepopovych/installsh

commit 5f1fe3f1f83717134b2be776609950682b9a5d87

installsh: Get rid of specific chmod reference file option
It might not be available on in non-GNU coreutils version of chmod(1).
Instead add -p option to cp(1) to copy mode from source file to
destination (temporary) file.

Signed-off-by: Serhey Popovych <serhe.popovych@gmail.com>

install.sh
@@ -619,15 +619,14 @@ reg_file_copy()
 	else
 		if [ -n "$DO_SUBST_TEMPLATES" ]; then
 			# Copy source to temporary destination
-			t="$(mktemp "$d.XXXXXXXX")" && cp -fd "$s" "$t" &&
+			t="$(mktemp "$d.XXXXXXXX")" && cp -fdp "$s" "$t" &&
 				exec_vars L='' -- subst_templates "$t" || return
 
 			if [ -d "$d" ] || ! cmp -s "$t" "$d"; then
 				# Backup if needed before installing
 				install_sh__backup "$d" || return
 				# Hard link temporary file
-				cp -fd $CP_OPTS -l "$t" "$d" &&
-					chmod -f --reference="$s" "$d" || return
+				cp -fd $CP_OPTS -l "$t" "$d" || return
 			fi
 			rm -f "$t" || return
 		else

commit 7fbf4a440e6cba908b20f7acf2e1d9b7ef5a19d4

installsh: Do not preserve copied files ownership and timestamp
Copying files with cp(1) option -p will preserve mode, ownership and
timestamp while we only require mode as installation might be performed
by user other than one owning source file (e.g. by root user on target
system, while source owned by original user).

Fix by using stat(1) to get access rights in octal and pass them to
chmod(1) when we install new copy. This partially reverts commit
5f1fe3f1f837 ("installsh: Get rid of specific chmod reference file
option") with only exception that GNU coreutils cp(1) specific option
replaced with "$(stat -c '0%a' "$s")".

As side effect we add dependency on stat(1) to this implementation
that however should be available in most environment.

Fixes: 5f1fe3f1f837 ("installsh: Get rid of specific chmod reference file option")
Signed-off-by: Serhey Popovych <serhe.popovych@gmail.com>

install.sh
@@ -1,6 +1,6 @@
 #!/bin/sh -e
 
-REQUIRES='printf id mkdir rmdir ln cp mv rm readlink chown chmod cmp mktemp sort tr sed'
+REQUIRES='printf id stat mkdir rmdir ln cp mv rm readlink chown chmod cmp mktemp sort tr sed'
 
 # Usage: pass() [...]
 pass()
@@ -704,14 +704,16 @@ reg_file_copy()
 	else
 		if [ -n "$DO_SUBST_TEMPLATES" ]; then
 			# Copy source to temporary destination
-			t="$(mktemp "$d.XXXXXXXX")" && copy -fdp "$s" "$t" &&
+			t="$(mktemp "$d.XXXXXXXX")" && copy -fd "$s" "$t" &&
 				exec_vars L='' -- subst_templates "$t" || return
 
 			if [ -d "$d" ] || ! cmp -s "$t" "$d"; then
 				# Backup if needed before installing
 				install_sh__backup "$d" || return
 				# Hard link temporary file
-				copy -fdl $CP_OPTS "$t" "$d" || return
+				copy -fdl $CP_OPTS "$t" "$d" &&
+					chmod -f "$(stat -c '0%a' "$s")" "$d" ||
+					return
 			fi
 			rm -f "$t" || return
 		else